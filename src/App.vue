f3e4ef517137812ebe5a10612898826c23022cfa
kuriakosejohn <john.kuriakose@ymail.com>
2018-08-02 10:50:12 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <Canvas/>
+  </div>
+</template>
+
+<script>
+import Canvas from "./components/Canvas";
+
+export default {
+  name: "App",
+  components: {
+    Canvas
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="canvas">
+    <Pixel v-for="n in 30*30"/>
+  </div>
+  
+</template>
+
+<script>
+import Pixel from './Pixel'
+  
+export default {
+    name: 'App',
+  components: {
+    Pixel
+  }
+  }
+</script>
+
+<style scoped>
+.canvas {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 900px;
+}
+</style>
